bcda0981a9dd1daa3c615112e1a403f1e0094a49
Joshua Singleton <jsingle345@gmail.com>
2020-09-11 23:33:04 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -68,9 +68,7 @@ def run(my_songs)
         user_response = gets.chomp
       when "exit"
         exit_jukebox
-      else
-        puts "Invalid input, please try again:"
-        user_response = gets.chomp
+      
       end
     end
 end 
